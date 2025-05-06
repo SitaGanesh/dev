fa573da9425af6db54e08da3520898866e54729a
Sita Ganesh <sitaganesh07@gmail.com>
2025-05-06 20:11:47 +0530

    production updation

--- frontend1/src/components/Body/SideBar/CodeEditor/CodeEditor.jsx
+++ frontend1/src/components/Body/SideBar/CodeEditor/CodeEditor.jsx
@@ -3,8 +3,8 @@ import Editor from "@monaco-editor/react";
 import io from "socket.io-client";
 import axios from "axios";
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
-const EXECUTOR_URL = import.meta.env.VITE_EXECUTOR_URL || "http://localhost:8000";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:3001";
+const EXECUTOR_URL = import.meta.env.VITE_EXECUTOR_URL || "http://localhost:5000";
 
 const socket = io(SOCKET_URL);
 
